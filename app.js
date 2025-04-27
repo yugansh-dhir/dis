2d6f924462835731efa2d36630406251c003730b
unknown <yuganshdhir608@gmail.com>
2025-04-27 15:49:36 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const path = require("path");
+const ejsMate = require("ejs-mate");
+
+const app = express();
+
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+app.get("/videoEditing", function (req, res) {
+  res.render("videoEditing");
+});
+
+app.get("/web", function (req, res) {
+  res.render("web");
+});
+
+app.get("/growIndia", function (req, res) {
+  res.render("growIndia");
+});
+
+app.get("/royaltyish", function (req, res) {
+  res.render("royaltyish");
+});
+
+app.get("/freelancing", function (req, res) {
+  res.render("freelancing");
+});
+
+app.get("*", function (req, res) {
+  res.redirect("/");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, process.env.IP, function () {
+  console.log(`DIS website is live on port ${port}`);
+});
